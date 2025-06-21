c2169de5ff1117f3c4e1546bdf5eebf2e8d33bb7
JennyTan123 <85146823+JennyTan123@users.noreply.github.com>
2025-06-21 17:00:10 +0800

    Update Data Manager.py

--- Data_Manager.py
+++ Data_Manager.py
@@ -1,10 +1,6 @@
 import streamlit as st
-from utils.ingestion import data_ingestion, data_ingestion_window, data_ingestion_mysql #, load_data_into_vector_db
+from utils.ingestion import data_ingestion, data_ingestion_window, data_ingestion_mysql, load_data_into_vector_db
 from langchain.retrievers import BM25Retriever
-from tools.dataset_summary_tool import dataset_summary_async
-from tools.question_recommendation_tool import generate_question_recommendations_async
-import asyncio
-import re
 
 class DataManager:
     def __init__(self, app):
@@ -21,51 +17,17 @@ class DataManager:
          
             if st.session_state.data.name.endswith(".csv"): 
                 table_name, (df_cleaned, schema) = next(iter(results.items()))
-                self.schema = schema
-                self.df = df_cleaned
+                st.session_state.schema = schema
                 st.session_state.df = df_cleaned
-                st.session_state.file_name = re.sub(r'[^a-zA-Z0-9_-]', '_', st.session_state.data.name.split(".")[0])
+                st.session_state.file_name = st.session_state.data.name.split(".")[0]
                 self.app.logger.debug(f"Filename: ", st.session_state.file_name)
-                
-                # # Generate a sumamry & question recommendations for CSV
-                # num_rows = min(len(self.df), 11)
-                # sample_dataset = self.df[:num_rows]
-                # sample_dataset_str = sample_dataset.to_csv(sep="|", index=False, lineterminator="\n")
 
-                # # Main function to run tasks concurrently
-                # async def main(llm, sample_dataset_str, rows, cols, schema):
-                #     # Run tasks concurrently using asyncio.gather
-                #     self.app.logger.debug("Starting concurrent execution of summary and question recommendations...")
-                #     excel_summary, question_recommendations = await asyncio.gather(
-                #         dataset_summary_async(llm, sample_dataset_str, rows, cols, schema),
-                #         generate_question_recommendations_async(llm, sample_dataset_str)
-                #     )
-                #     self.app.logger.debug("Concurrent execution completed.")
-                #     return excel_summary, question_recommendations
-
-                # # Run the async main function
-                # excel_summary, question_recommendations = asyncio.run(
-                #     main(self.app.llm, sample_dataset_str, self.df.shape[0], self.df.shape[1], self.schema)
-                # )
-
-                # print("Question Recommendation")
-                # print(question_recommendations)
-
-                # self.app.logger.debug("Excel Summary:\n",excel_summary)
-                # self.app.logger.debug("Question Recommendation: \n",question_recommendations)
-
-                # for key, value in {"excel_summary": excel_summary, "question_recommendations": question_recommendations}.items():
-                #     if value:
-                #         st.session_state[key] = value
-
-            # if st.session_state.data.name.endswith(".xlsx"): 
-            #     st.session_state.file_name = st.session_state.data.name
-            #     self.app.logger.info("Load Excel Data into Vector DB...")
-            #     self.app.logger.info(structured_excel_elements)
-            #     st.session_state.vector_store = load_data_into_vector_db(excel_structured_documents=structured_excel_elements, collection_name="excel_data")
-            #     st.session_state.bm25_retriever = BM25Retriever.from_documents(structured_excel_elements)
-            #     st.session_state.bm25_retriever.k = 3
-            #     self.app.logger.info("Completed loading Excel Data into Vector DB.")
+            if st.session_state.data.name.endswith(".xlsx"): 
+                self.app.logger.info("Load Excel Data into Vector DB...")
+                st.session_state.vector_store = load_data_into_vector_db(excel_structured_documents=structured_excel_elements, collection_name="excel_data")
+                st.session_state.bm25_retriever = BM25Retriever.from_documents(structured_excel_elements)
+                st.session_state.bm25_retriever.k = 3
+                self.app.logger.info("Completed loading Excel Data into Vector DB.")
                
         except Exception as e:
             st.error(f"Error loading the file: {e}")
